2cbee355f8620c388d5aeb292668ac9ae2910a7c
hrosa <hdavidrosa@gmail.com>
2018-02-22 18:57:11 +0000

    RMS-29 Migrated CachedRemoteStreamProvider to Spring
    RMS-30 Migrated DirectRemoteStreamProvider to Spring

--- bootstrap/src/main/java/org/restcomm/media/server/standalone/bootstrap/ioc/spring/SpringCoreConfiguration.java
+++ bootstrap/src/main/java/org/restcomm/media/server/standalone/bootstrap/ioc/spring/SpringCoreConfiguration.java
@@ -19,7 +19,7 @@
  *  02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
 
-package org.restcomm.media.server.standalone.bootstrap.ioc.spring.core;
+package org.restcomm.media.server.standalone.bootstrap.ioc.spring;
 
 import com.google.common.util.concurrent.ListeningScheduledExecutorService;
 import com.google.common.util.concurrent.MoreExecutors;
