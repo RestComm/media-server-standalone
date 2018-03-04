32224084d1904e7572d294dd380955ab2b616f91
hrosa <hdavidrosa@gmail.com>
2018-03-04 14:56:45 +0000

    RMS-68 Added injection of SSL Configuration. No more need to override JAVA_OPTS in startup script

--- assembly/src/main/config/autoconfig/verify-dependencies.sh
+++ assembly/src/main/config/autoconfig/verify-dependencies.sh
@@ -3,19 +3,21 @@
 ## Author: Henrique Rosa (henrique.rosa@telestax.com)
 
 verifyJava() {
-    if [ -z "$(which java)" ]; then
-        echo "Java dependency is missing."
-        echo "CentOS/RHEL: java-1.8.0-openjdk-devel.x86_64"
+    if [[ -z "$(which java)" ]] || [[ $(java -version) != "1.8.*" ]]; then
+        echo "Java 8 dependency is missing."
+        echo "CentOS/RHEL: yum install java-1.8.0-openjdk-devel"
         echo "Debian/Ubuntu:"
         echo "    add-apt-repository ppa:openjdk-r/ppa"
         echo "    apt-get update"
         echo "    apt-get install openjdk-8-jdk"
-        echo "macOS: brew cask install java8"
+        echo "macOS: "
+        echo "    brew tap caskroom/versions"
+        echo "    brew cask install java8"
         exit 1
     fi
 }
 
-verifyScreen() {
+verifyTmux() {
     if [ -z "$(which tmux)" ]; then
         echo "tmux dependency is missing."
         echo "CentOS/RHEL: yum install tmux"
@@ -25,16 +27,5 @@ verifyScreen() {
     fi
 }
 
-verifyXmlstarlet() {
-    if [ -z "$(which xmlstarlet)" ]; then
-        echo "XML Starlet dependency is missing."
-        echo "CentOS/RHEL: yum install xmlstarlet"
-        echo "Debian/Ubuntu: apt-get install xmlstarlet"
-        echo "macOS: brew install xmlstarlet"
-        exit 1
-    fi
-}
-
 verifyJava
-verifyScreen
-verifyXmlstarlet
+verifyTmux
